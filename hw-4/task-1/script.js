f694969692a887e9b8f321e0a826441fc24726d2
Kan-Alex <kan.aliosha@gamil.com>
2022-08-15 10:55:10 +0300

    redacting hw-4-task-1

--- hw-4/task-1/script.js
+++ hw-4/task-1/script.js
@@ -1,11 +1,15 @@
-const word = 'localization'
+const words = ['localization', 'Uncopyrightable', 'word', 'Trichotillomania', '1', 'Unimaginatively', 'aboba', 'Sesquipedalianism', 'Tergiversation']
 
-const interval = word.length - 2
+for (const word of words) {
+                       
+    const interval = word.length - 2
 
-function lastletter(arr) {
-    return arr[arr.length - 1]
-}
+    function lastletter(arr) {
+        return arr[arr.length - 1]
+    }
+    
+    if(word.length > 10) {
+        console.log(word[0] + interval + lastletter(word))
+    }
 
-if (word.length > 10) {
-    console.log(word[0] + interval + lastletter(word))
 }
